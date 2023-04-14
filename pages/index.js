440650f7af3caed54e7306a6267baff9cf17e845
Giorgi <chikviladze555@gmail.com>
2023-04-14 10:46:36 +0400

    Layout, products and quantities

--- pages/index.js
+++ pages/index.js
@@ -1,12 +1,15 @@
-import Image from 'next/image';
-import { Inter } from 'next/font/google';
-
-const inter = Inter({ subsets: ['latin'] });
+import data from '../utils/data';
+import Layout from '../components/Layout';
+import ProductItem from '../components/ProductItem';
 
 export default function Home() {
   return (
-    <main className="text-3xl font-bold">
-      <>Next Tailwind Chamazona</>
-    </main>
+    <Layout title="Home Page">
+      <div className="grid grid-cols-1 gap-4 md:grid-cols-3 lg:grid-cols-4">
+        {data.products.map((product) => (
+          <ProductItem product={product} key={product.slug}></ProductItem>
+        ))}
+      </div>
+    </Layout>
   );
 }
