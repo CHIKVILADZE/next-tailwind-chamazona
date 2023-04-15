ca5dfc197e3442b145aea95820740bd9b6f49db2
Giorgi <chikviladze555@gmail.com>
2023-04-15 19:38:00 +0400

    Seed sample products

--- pages/api/seed.js
+++ pages/api/seed.js
@@ -1,12 +1,15 @@
 import db from '@/utils/db';
 import User from '@/models/User';
+import Product from '@/models/Product';
 import data from '@/utils/data';
 
 const handler = async (req, res) => {
   await db.connect();
   await User.deleteMany();
   await User.insertMany(data.users);
+  await Product.deleteMany();
+  await Product.insertMany(data.products);
   await db.disconnect();
-  res.send({ message: 'sedded succesfully' });
+  res.send({ message: 'seeded succesfully' });
 };
 export default handler;
--- utils/Store.js
+++ utils/Store.js
@@ -6,7 +6,7 @@ export const Store = createContext();
 const initialState = {
   cart: Cookies.get('cart')
     ? JSON.parse(Cookies.get('cart'))
-    : { cartItems: [], shippingAdress: {} },
+    : { cartItems: [], shippingAdress: {}, paymentMethod: '' },
 };
 
 function reducer(state, action) {
