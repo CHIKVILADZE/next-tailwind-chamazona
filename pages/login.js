07acf219caad8b75bbf36afa7f335fe653ee7aad
Giorgi <chikviladze555@gmail.com>
2023-04-15 12:31:39 +0400

    Create Login Api

--- pages/login.js
+++ pages/login.js
@@ -2,8 +2,23 @@ import Layout from '@/components/Layout';
 import React from 'react';
 import Link from 'next/link';
 import { useForm } from 'react-hook-form';
+import { signIn, useSession } from 'next-auth/react';
+import { getError } from '@/utils/error';
+import { toast } from 'react-toastify';
+import { useEffect } from 'react';
+import { useRouter } from 'next/router';
 
 export default function LoginScreen() {
+  const { data: session } = useSession();
+  const router = useRouter();
+  const { redirect } = router.query;
+
+  useEffect(() => {
+    if (session?.user) {
+      router.push(redirect || '/');
+    }
+  }, [router, session, redirect]);
+
   const {
     register,
     handleSubmit,
@@ -11,8 +26,19 @@ export default function LoginScreen() {
     formState: { errors },
   } = useForm();
 
-  const submitHandler = ({ email, password }) => {
-    console.log(email, password);
+  const submitHandler = async ({ email, password }) => {
+    try {
+      const result = await signIn('credentials', {
+        redirect: false,
+        email,
+        password,
+      });
+      if (result.error) {
+        toast.error(result.error);
+      }
+    } catch (err) {
+      toast.error(getError(err));
+    }
   };
 
   return (
